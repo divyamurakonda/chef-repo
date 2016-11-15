4bcc7cccb4cbf373de7ffba59bf3ffd780d23e33
divyamurakonda <murakondadivya@gmail.com>
2016-11-15 11:43:03 -0600

    fire

---
+++ chef-repo/cookbooks/awesome_customers_ubuntu/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: awesome_customers_ubuntu
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'apt::default'
+include_recipe 'awesome_customers_ubuntu::firewall'
