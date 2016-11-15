97ac56173b947c8d121eaa3932ab7fad94343635
divyamurakonda <murakondadivya@gmail.com>
2016-11-15 13:42:32 -0600

    rewf

--- chef-repo/cookbooks/awesome_customers_ubuntu/attributes/default.rb
+++ chef-repo/cookbooks/awesome_customers_ubuntu/attributes/default.rb
@@ -6,6 +6,3 @@ end
 default['awesome_customers_ubuntu']['user'] = 'web_admin'
 default['awesome_customers_ubuntu']['group'] = 'web_admin'
 default['awesome_customers_ubuntu']['document_root'] = '/var/www/customers/public_html'
-  
-normal_unless['awesome_customers_ubuntu']['database']['root_password'] = random_password
-normal_unless['awesome_customers_ubuntu']['database']['admin_password'] = random_password
